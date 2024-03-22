01b14e2fe3e6aad3bfcd37da99d9d5b23225e9ea
Ashish Patel <ashi.patel546@gmail.com>
2024-03-22 18:40:59 +0530

    for throttler cron, old timestamp updated for 30 min. Now 30 min old IPs will be delted.

--- src/throttler/throttler.service.ts
+++ src/throttler/throttler.service.ts
@@ -23,24 +23,24 @@ export class ThrottlerService {
     this.requestCounts.delete(identifier);
   }
 
+  //Method to add entry for IP
   addEntry(identifier: string) {
     this.logger.log(`Adding IP to throttler: ${identifier}`);
     this.requestCounts.set(identifier, { count: 1, timestamp: new Date() });
   }
-
 
   @Cron(CronExpression.EVERY_10_MINUTES)
   deleteOldIpEntry(cleanAll: boolean = false) {
     if (cleanAll) {
       this.logger.log('Cleaning all cache');
       this.requestCounts.clear();
-      return
+      return;
     }
     this.logger.log('Cleaning throttler old cache');
-    this.logger.log(`Cleaning at : ${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+    this.logger.log(`Cleaning at : ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
     for (let [key, value] of this.requestCounts) {
       const { timestamp } = value;
-      const timeStampToCompare = moment().subtract(1, 'h');
+      const timeStampToCompare = moment().subtract(30, 'm');
       if (moment(timestamp).isSameOrBefore(timeStampToCompare)) {
         this.logger.log('Deleting old entries from IP');
         this.deleteEntry(key);
